29b84acd08a4079d7689129b3707b5cd4d609c39
luigigorlero <luigigorlero@MacBook-Pro-di-Gigi.local>
2017-02-10 15:41:00 +0100

    3d lupa

--- 3d/script.js
+++ 3d/script.js
@@ -1,28 +1,29 @@
+var w = window.innerWidth,
+    h = window.innerHeight,
+    halfWidth = w / 2,
+    halfHeight = h / 2;
 var scene = new THREE.Scene();
-var aspect = window.innerWidth / window.innerHeight;
+var aspect = w / h;
 var camera = new THREE.PerspectiveCamera(75, aspect, 1, 100);
 var renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-console.log(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement); 
-var parallaxX = 0,
-    parallaxY = 0,
-    rotationX = 0,
-    rotationY = 0;
-var geometry = new THREE.BoxGeometry(1, 10, 1);
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+var radiantX = 0,
+    radiantY = 0,
+    radius = 5;
+var geometry = new THREE.BoxGeometry(3, 5, 3);
 var material = new THREE.MeshNormalMaterial();
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 5);
 
 var render = function() {
     requestAnimationFrame(render);
-    camera.position.x = parallaxX;
-    camera.position.y = parallaxY;
-    cube.rotation.x = rotationX;
-    cube.rotation.y = rotationY;
-    camera.lookAt(cube.position);
+    camera.position.x = Math.sin(radiantX) * radius;
+    camera.position.y = Math.sin(radiantY) * radius;
+    camera.position.z = (Math.cos(radiantX) + Math.cos(radiantY)) + radius;
 
+    camera.lookAt(cube.position);
     renderer.render(scene, camera);
 }
 
@@ -32,17 +33,20 @@ render();
 
 
 document.onmousemove = mousePosition;
+//document.onresize = windowResize;
+
+function windowResize(e) {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    halfWidth = w / 2;
+    halfHeight = h / 2;
+}
 
 function mousePosition(e) {
-    var mouseX = e.pageX;
-    var mouseY = e.pageY;
+    var mouseX = e.pageX - halfWidth;
+    var mouseY = - (e.pageY - halfHeight);
 
-    var sceneX = - (window.innerWidth / 2 - mouseX);
-    var sceneY = window.innerHeight / 2 - mouseY;
+    radiantX = mouseX / halfWidth * Math.PI / 2;
+    radiantY = mouseY / halfHeight * Math.PI / 2;
 
-    parallaxX = sceneX / 100;
-    parallaxY = sceneY / 75;
-
-    rotationX = sceneX / 1000;
-    rotationY = - (sceneY / 1000);
 }
